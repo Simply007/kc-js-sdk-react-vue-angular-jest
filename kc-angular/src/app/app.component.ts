4b5af6e38be81443c2e7cc3fee123be0a78542ec
Simply007 <chrastina.ondra@gmail.com>
2019-04-09 13:18:35 -0400

    load data from KC in angular

--- kc-angular/src/app/app.component.ts
+++ kc-angular/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { DeliveryClient, IDeliveryClient, ContentItem } from 'kentico-cloud-delivery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  client: IDeliveryClient;
+  articles: ContentItem[];
+  loaded: boolean;
   title = 'kc-angular';
+
+  constructor() {
+    this.client = new DeliveryClient({ projectId: '975bf280-fd91-488c-994c-2f04416e5ee3' });
+    this.loaded = false;
+    this.articles = [];
+  }
+
+  ngOnInit(): void {
+    this.client.items()
+      .type('article')
+      .getPromise()
+      .then(result => {
+        // eslint-disable-next-line no-console
+        console.log(result.items);
+        this.articles = result.items;
+        this.loaded = true;
+      });
+  }
 }
