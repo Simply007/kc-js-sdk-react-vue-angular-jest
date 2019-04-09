b83fd845e6554bfdc369a901ce8f1eeb9ef8cd9c
Simply007 <chrastina.ondra@gmail.com>
2019-04-09 11:21:59 -0400

    load data from kentico cloud

--- kc-react/src/App.js
+++ kc-react/src/App.js
@@ -2,8 +2,35 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { DeliveryClient } from 'kentico-cloud-delivery';
+
+const client = new DeliveryClient({ projectId: "975bf280-fd91-488c-994c-2f04416e5ee3" });
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false,
+      articles: []
+    };
+  }
+
+  componentDidMount() {
+    client.items()
+      .type("article")
+      .getPromise()
+      .then(result => {
+        console.log(result.items);
+        this.setState({
+          loaded: true,
+          articles: result.items
+        });
+      })
+  }
+
+
   render() {
+    const articles = this.state.articles.map(article => (<li>{article.elements.title.value}</li>));
     return (
       <div className="App">
         <header className="App-header">
@@ -11,6 +38,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          {this.state.loaded && (<ul>{articles}</ul>)}
           <a
             className="App-link"
             href="https://reactjs.org"
